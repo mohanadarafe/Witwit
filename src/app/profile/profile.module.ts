2c658a86d44a24379453cf360c975945eedd4f28
Caren Rizk <karen.rizk1997@gmail.com>
2019-01-19 18:31:08 -0500

    added roouting module and config

--- src/app/profile/profile.module.ts
+++ src/app/profile/profile.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ProfileComponent } from './pages/profile.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ProfileComponent
+  ],
   imports: [
     CommonModule
   ]
 })
-export class RoutingModule { }
+export class ProfileModule { }
