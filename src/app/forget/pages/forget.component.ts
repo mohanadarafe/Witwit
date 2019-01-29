0a52c2fe181b263865764ef7f4d21dc929900e1b
Daphne <daphne.augier@gmail.com>
2019-01-29 00:21:31 -0500

    Update forget.component.ts

--- src/app/forget/pages/forget.component.ts
+++ src/app/forget/pages/forget.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../shared/services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-forget',
