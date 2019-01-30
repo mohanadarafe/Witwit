0505803cbdd45a866f759640a3e1fdbb29bc53e3
Alain D. Jobs <33582344+AlainJobs@users.noreply.github.com>
2019-01-29 22:00:12 -0500

    Register Form with input validation

--- src/app/register/pages/register.component.ts
+++ src/app/register/pages/register.component.ts
@@ -1,19 +1,50 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../shared/services/auth.service';
 import { Router } from '@angular/router';
+
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+// import custom validator to validate that password and confirm password fields match
+import { MustMatch } from 'src/app/_helpers/must-match.validator';
+
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-  registeredUser = {};
-  constructor(private auth: AuthService,
-    private router: Router) {}
+  registerForm: FormGroup;
+  submitted = false;
+  registeredUser = {}; //I think this is Hampic's doing. A.J
 
-  ngOnInit() {}
+  constructor(private formBuilder: FormBuilder) { }
 
+  ngOnInit() {
+    this.registerForm = this.formBuilder.group({
+      Username: ['', Validators.required],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      confirmPassword: ['', Validators.required],
+      userAge: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]]
+    }, {
+        validator: MustMatch('password', 'confirmPassword')
+      });
+  }
+
+  /**********************
+   *  Alain Jobs
+   **********************/
+  //constructor(private auth: AuthService,
+  // private router: Router) {} 
+
+  //--> // Sorry Haboub, but multiple contructor init is not allowed. We will have to find a workaround. :S
+  //A. J
+
+  /*    
   registerUser() {
+    this.registerForm = this.formBuilder.group({
+
+    })
     this.auth.registerUser(this.registeredUser).subscribe(
       res => {
         localStorage.setItem('token', res.token);
@@ -21,5 +52,20 @@ export class RegisterComponent implements OnInit {
       },
       err => console.log(err)
     );
+  }*/
+
+
+  // convenience getter for easy access to form fields
+  get g() { return this.registerForm.controls; }
+
+  onSubmit() {
+    this.submitted = true;
+
+    //--> // stop here if form is invalid --> maybe starts from there to link to the database?
+    if (this.registerForm.invalid) {
+      return;
+    }
+
+    alert('SUCCESS!! :-)\n\n' + JSON.stringify(this.registerForm.value)) //Just to test!
   }
 }
