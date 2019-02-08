8654667442e8c208e87d456b06df31f1478677f1
carenrizk <karen.rizk1997@gmail.com>
2019-02-08 15:39:49 -0500

    display wits dynamically

--- src/app/timeline/pages/timeline.component.ts
+++ src/app/timeline/pages/timeline.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, ViewChild, ElementRef } from "@angular/core";
 import { TimelineService } from "../services/timeline.service";
 import { MatSnackBar } from '@angular/material';
+import { faHeart, faHeartBroken } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: "app-timeline",
@@ -11,30 +12,32 @@ export class TimelineComponent implements OnInit {
   witObject = {};
   @ViewChild('witPost') witPost: ElementRef;
   wits: any;
+  faHeart = faHeart;
+  faHeartBroken = faHeartBroken;
 
   constructor(private timelineService: TimelineService,
     private snackBar: MatSnackBar) {}
 
   ngOnInit() {
     this.timelineService.pullWit().subscribe(
-      res => {
+      (res) => {
         this.wits = res;
         console.log(this.wits);
       },
-      err => console.log("error")
+      (err) => console.log("error", err)
     );
   }
 
   submitWit(value: string) {
     this.witObject["wit"] = value;
     this.timelineService.postWit(this.witObject).subscribe(
-      res => {
+      (res) => {
         this.witPost.nativeElement.value = '';
           this.snackBar.open('Wit posted successfully', 'ok', {
             duration: 3000,
           });
       },
-      err => {
+      (err) => {
         this.snackBar.open('Error posting wit', 'ok', {
           duration: 3000,
         });
