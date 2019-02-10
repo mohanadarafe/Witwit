bb231795ba146d1573a6240c40f4f9236fc74bc3
Robert Beaudenon <robert.beaudenon@mail.mcgill.ca>
2019-02-09 19:21:46 -0500

    Backend for the timeline profile
    Created the backend of the timeline profile to populate the informations of the logged in user.

--- server/routes/login_register.js
+++ server/routes/login_register.js
@@ -7,7 +7,7 @@ userLoggedIN = "Hampic";
 var connection = mySql.createConnection({
   host: "127.0.0.1",
   user: "root",
-  password: "montreal",
+  password: "",
   database: "witwit",
   port:"3306"
 });
--- server/server.js
+++ server/server.js
@@ -16,21 +16,35 @@ var connection = mySql.createConnection({
 app = express()
 app.use(cors())
 app.use(bodyParser.json())
+
+
+//To be able to use the APIs:
+
 //To be able to use the login-register API
 const loginRegisterApi = require('./routes/login_register')
 app.use('/routes/login_register', loginRegisterApi)
 
+//To be able to use the timeline API
 const timelineApi = require('./routes/timeline')
 app.use('/routes/timeline', timelineApi)
 
+//To be able to use the witPost API
 const witPostApi = require('./routes/witPost')
 app.use('/routes/witPost', witPostApi)
 
-//To make suer it is wokring and on which port:
+//To be able to use the timelineProfile API
+const timelineProfileApi = require('./routes/timelineProfile')
+app.use('/routes/timelineProfile', timelineProfileApi)
+
+
+//To make sure the server is working : will be displayed by typing on the web 'http://localhost:3002/'
 app.get('/', (req, res) => {
   res.send("I am the server ")
 })
 
+
+
+//To make sure that the database is connected
 connection.connect(function (err) {
   if (err) throw err;
   console.log("Connected to DB")
@@ -40,6 +54,7 @@ connection.connect(function (err) {
   })
 })
 
+//To make sure that the server is working and on which port : will be displayed in the terminal
 app.listen(PORT, function () {
   console.log("The server is working on port: " + PORT)
 })
