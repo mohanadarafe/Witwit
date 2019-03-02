0ee052ad348a0a6ebb1a3a01286b02c6be550648
mohanadarafe <marafe@live.com>
2019-03-02 18:52:25 -0500

    Test Case 1: liking twice

--- tests.js
+++ tests.js
@@ -3,9 +3,12 @@
 */
 
 
-//Testing Functionality
-var test = false;
-if(test)
-    console.log("test passed!");
-else
-    console.log("test failed!");
+/*
+ *Test Case 1: Liking a post more than once
+ *
+ * 
+ *Pass Case: 0 likes -> 1 likes -> 0 likes
+ *Fail Case: 0 likes -> 1 likes -> 2 likes
+*/
+const likeWit = require('./server/routes/timeline');
+
