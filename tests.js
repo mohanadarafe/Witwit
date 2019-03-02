51c6519af6f27a0a59059e4aae7be9c5bde3ef3c
mohanadarafe <marafe@live.com>
2019-03-02 18:53:59 -0500

    Test Case 1: liking twice

--- tests.js
+++ tests.js
@@ -5,10 +5,18 @@
 
 /*
  *Test Case 1: Liking a post more than once
- *
+ *Author: Mohanad
  * 
  *Pass Case: 0 likes -> 1 likes -> 0 likes
  *Fail Case: 0 likes -> 1 likes -> 2 likes
 */
 const likeWit = require('./server/routes/timeline');
 
+
+/*
+ *Test Case 2: Posting an empty Wit
+ *Author: Mohanad
+ * 
+ *Pass Case: Subscribe function works
+ *Fail Case: Subscribe function fails
+*/
