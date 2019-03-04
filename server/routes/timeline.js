17d7d2cd3b644df892606107c6f6acb8043be8f8
Hambrsoom <hampico1234@gmail.com>
2019-03-04 08:46:44 -0500

    #83 liking a wit of a not followed user

--- server/routes/timeline.js
+++ server/routes/timeline.js
@@ -1,74 +1,98 @@
 const express = require("express");
 const router1 = express.Router();
-const sourceFile = require('./login_register')
-var mySql = require("mysql");
 const jwtToken = require('jwt-decode');
 const connection = require('../server');
 var userLoggedIN = null;
 
 
 //revealing the posts.
-router1.post('/timeline', (req, res) => {
 
+router1.post('/timeline', (req, res)=> {
   userToken = req.body;
-
+  userLoggedIN = "Hampic";
   if(userLoggedIN==null){
     var decoded = (jwtToken(userToken.token)).username;
     userLoggedIN = decoded;
   }
-//First checking if the user is following anyone. if he doesn't then we will only display his own post on the timeline if he has any:
-  sqlQuery1_temp = "Select following FROM users Where username =?";
-  connection.connection.query(sqlQuery1_temp, userLoggedIN, (err, row) => {
+  sqlTimelineQuery = "Select * FROM events"
+  connection.connection.query(sqlTimelineQuery, (err, answer)=>{
     if (err) {
       res.json({
         code: 400,
         message: "there are some error with query"
       });
     }
-    else {
- //if it has followings: then we will display the wits of his followings and his.
-      if (row[0].following >= 0) {
-        sqlQuery1 = "SELECT * FROM events WHERE username IN (SELECT follow_name AND username FROM following WHERE username = ?)"
-        connection.connection.query(sqlQuery1, userLoggedIN, function (err, results) {
-          if (err) {
-            res.json({
-              code: 400,
-              message: "there are some error with query"
-            });
-          }
-          else {
-            if (results.length > 0) {
-              res.status(200).send(results);
-            }
-            else {
-              res.status(400).json("No wits to show 1");
-            }
-          }
-        })
-      }
-      else {
-//Otherwise we will show only his:
-        sqlQuery_no_following = "SELECT * FROM events WHERE username = ?";
-        connection.connection.query(sqlQuery_no_following, userLoggedIN,  (err1, rowss)=> {
-          if(err1) {
-            res.json({
-              code: 400,
-              message: "there are some error with query"
-            });
-          }
-          else{
-                if (rowss.length > 0) {
-                  res.status(200).send(rowss);
-                }
-                else {
-                  res.status(400).json("No wits to show 2");
-                }
-          }
-        })
-      }
+    else if(answer.length ==0) {
+      res.status(200).json("No wits to show");
+    }
+    else{
+      res.status(200).send(answer);
     }
   })
 })
+
+
+// router1.post('/timeline', (req, res) => {
+
+//   userToken = req.body;
+
+//   if(userLoggedIN==null){
+//     var decoded = (jwtToken(userToken.token)).username;
+//     userLoggedIN = decoded;
+//   }
+// //First checking if the user is following anyone. if he doesn't then we will only display his own post on the timeline if he has any:
+//   sqlQuery1_temp = "Select following FROM users Where username =?";
+//   connection.connection.query(sqlQuery1_temp, userLoggedIN, (err, row) => {
+//     if (err) {
+//       res.json({
+//         code: 400,
+//         message: "there are some error with query"
+//       });
+//     }
+//     else {
+//  //if it has followings: then we will display the wits of his followings and his.
+//       if (row[0].following >= 0) {
+//         sqlQuery1 = "SELECT * FROM events WHERE username IN (SELECT follow_name AND username FROM following WHERE username = ?)"
+//         connection.connection.query(sqlQuery1, userLoggedIN, function (err, results) {
+//           if (err) {
+//             res.json({
+//               code: 400,
+//               message: "there are some error with query"
+//             });
+//           }
+//           else {
+//             if (results.length > 0) {
+//               res.status(200).send(results);
+//             }
+//             else {
+//               res.status(400).json("No wits to show 1");
+//             }
+//           }
+//         })
+//       }
+//       else {
+// //Otherwise we will show only his:
+//         sqlQuery_no_following = "SELECT * FROM events WHERE username = ?";
+//         connection.connection.query(sqlQuery_no_following, userLoggedIN,  (err1, rowss)=> {
+//           if(err1) {
+//             res.json({
+//               code: 400,
+//               message: "there are some error with query"
+//             });
+//           }
+//           else{
+//                 if (rowss.length > 0) {
+//                   res.status(200).send(rowss);
+//                 }
+//                 else {
+//                   res.status(400).json("No wits to show 2");
+//                 }
+//           }
+//         })
+//       }
+//     }
+//   })
+// })
 
 
 router1.get('/likedWits', (req, res) => {
