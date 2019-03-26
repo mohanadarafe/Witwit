f18c9c87aeb21ba8f6a7120088ce5ba0a0f138f3
Dariush Noorzada <dariush.noorzada@mail.concordia.ca>
2019-03-26 16:22:28 -0400

    Set up services for dynamic autocomplete search

---
+++ src/app/toolbar/pages/search/services/search.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SearchService {
+
+  private userDynSearch = 'http://localhost:3002/routes/main_pages/searchEngine/dropDownList';
+
+  constructor(private http: HttpClient) { }
+
+  dropDownUsers(user) {
+    return this.http.post<any>(this.userDynSearch, name);
+  }
+}
